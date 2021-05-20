f5c122d12e55d0553e83426f4507fd23595565c9
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-20 10:49:57 +0900

    Observableに変換して結果を取り出すテスト

--- Assets/Scripts/TaskTest.cs
+++ Assets/Scripts/TaskTest.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
+using System;
+using UniRx;
 
 /// <summary>
 /// Taskの実験
@@ -14,10 +16,15 @@ public class TaskTest : MonoBehaviour
 
         var TestTask = await Test2();
         Debug.Log(TestTask);
+
+        // awaitしない版
+        var Test2Observable = Test2().ToObservable<string>();
+        Test2Observable.Subscribe((Str) => Debug.Log("ToObservable: " + Str));
     }
 
     /// <summary>
     /// テストその１
+    /// 単純に３秒待つだけ
     /// </summary>
     private async UniTask Test1()
     {
@@ -28,6 +35,7 @@ public class TaskTest : MonoBehaviour
 
     /// <summary>
     /// テストその２
+    /// ３秒待って文字列を返す
     /// </summary>
     /// <returns>文字列</returns>
     private async UniTask<string> Test2()
