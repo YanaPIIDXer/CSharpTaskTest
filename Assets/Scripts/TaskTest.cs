94463ba0134f3d804c541a73b15397d34a73ff41
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-20 11:25:38 +0900

    コルーチンからUniTaskへの変換

--- Assets/Scripts/TaskTest.cs
+++ Assets/Scripts/TaskTest.cs
@@ -17,8 +17,11 @@ public class TaskTest : MonoBehaviour
         var TestTask = await Test2();
         Debug.Log(TestTask);
 
+        var Test3Result = await Test3();
+        Debug.Log(Test3Result);
+
         // awaitしない版
-        var Test2Observable = Test2().ToObservable<string>();
+        var Test2Observable = Test3().ToObservable<string>();
         Test2Observable.Subscribe((Str) => Debug.Log("ToObservable: " + Str));
 
         Debug.Log("All Completed!");
@@ -44,5 +47,27 @@ public class TaskTest : MonoBehaviour
     {
         await UniTask.Delay(3000);
         return "End Task2";
+    }
+
+    /// <summary>
+    /// テストその３
+    /// コルーチンからの変換
+    /// </summary>
+    /// <returns>文字列</returns>
+    private UniTask<string> Test3()
+    {
+        var Obs = Observable.FromCoroutine<string>((o) => Test3Coroutine(o));
+        return Obs.ToUniTask<string>();
+    }
+
+    /// <summary>
+    /// Test3用のコルーチン
+    /// </summary>
+    /// <param name="Observer">Observer</param>
+    private IEnumerator Test3Coroutine(IObserver<string> Observer)
+    {
+        yield return new WaitForSeconds(3.0f);      // 通信待ちみたいな？
+        Observer.OnNext("Test3Coroutine Completed!");
+        Observer.OnCompleted();
     }
 }
